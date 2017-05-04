30b7210cc8de82e280d805eb4e31b37a08de5218
Ryan Day <rdanielday@users.noreply.github.com>
2017-05-03 21:20:17 -0400

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var express = require("express");
 // create our app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(3000, function () {
-    console.log("Server has started..............");
+app.use(function (req, res, next){
+  if (req.headers["x-forwarded-proto"] === "http") {
+    next();
+  } else {
+    res.redirect("http://" + req.hostname + req.url);
+  }
+});
+
+app.listen(PORT, function () {
+    console.log("Server is up on port " + PORT);
 });
