458310891568a34423a3655064305643c1ca4cf8
Ryan Day <rdanielday@gmail.com>
2017-05-04 20:26:19 +0000

    Added content to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,19 @@ var React = require("react");
 var About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
-      <p>This app was created for the Complete React Web Develop Course</p>
+      <h3 className="text-center">About This App</h3>
+      <p>
+        This weather app was built using React by Ryan Day for 
+        the <a href="http://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/content">
+        Complete React Web Developer Course</a>.
+      </p>
+      <p>
+        Ryan can be found on <a href="http://github.com/rdanielday">GitHub</a>  
+        and <a href="http://twitter.com/rdanielday">Twitter</a>.
+      </p>
+      <p>
+        Weather data provided by <a href="http://www.openweathermap.org">OpenWeatherMap</a>
+      </p>
     </div>
       );
 };
