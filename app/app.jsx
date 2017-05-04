3abefc9b7ece0f8a2570d9a51afc97987978e617
Ryan Day <rdanielday@gmail.com>
2017-05-04 16:43:23 +0000

    Added Foundation framework for app styling + other modules for use of Foundation

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,10 @@ var {
     IndexRoute,
     hashHistory
     } = require("react-router");
+    
+// load Foundation
+require("style!css!foundation-sites/dist/foundation.min.css");
+$(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
