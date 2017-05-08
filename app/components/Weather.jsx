a728843954b55bdd90caaf192b51709e6b41d4c3
Ryan Day <rdanielday@gmail.com>
2017-05-08 16:30:45 +0000

    Add error modal.

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,32 +1,38 @@
 var React          = require("react"),
     WeatherForm    = require("WeatherForm"),
     WeatherMessage = require("WeatherMessage"),
+    ErrorModal     = require("ErrorModal"),
     openWeatherMap = require("openWeatherMap");
 
 var Weather = React.createClass({
   getInitialState: function () {
     return {
       isLoading: false
-    }
+    };
   },
   handleSearch: function (location) {
     var that = this;
     
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
     
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
         temp: temp,
         isLoading: false
-      })
-    }, function (errorMessage) {
-      that.setState({isLoading: false});
-      alert(errorMessage);
+      });
+    }, function (e) {
+      that.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
     }); 
   },
   render: function () {
-    var {isLoading, temp, location} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
     
     function renderMessage () {
       if (isLoading) {
@@ -36,11 +42,20 @@ var Weather = React.createClass({
       }
     }
     
+    function renderError () {
+      if (typeof errorMessage === "string") {
+        return (
+          <ErrorModal message={errorMessage} />
+        );
+      }
+    }
+    
     return (
       <div>
         <h1 className="text-center">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch} />
         {renderMessage()}
+        {renderError()}
       </div>
     );
   }
