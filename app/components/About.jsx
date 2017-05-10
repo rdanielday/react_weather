ef13ee260d642f643e387082c82cde7c279a674f
Ryan Day <rdanielday@gmail.com>
2017-05-10 21:03:29 +0000

    finished nav search functionality

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,13 +5,10 @@ var About = (props) => {
     <div>
       <h3 className="text-center page-title">About This App</h3>
       <p>
-        This weather app was built using React by Ryan Day for 
-        the <a href="http://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/content">
-        Complete React Web Developer Course</a>.
+        This weather app was built using React by Ryan Day for the <a href="http://www.udemy.com/the-complete-react-web-app-developer-course/learn/v4/content">Complete React Web Developer Course</a>.
       </p>
       <p>
-        Ryan can be found on <a href="http://github.com/rdanielday">GitHub</a>  
-        and <a href="http://twitter.com/rdanielday">Twitter</a>.
+        Ryan can be found on <a href="http://github.com/rdanielday">GitHub</a> and <a href="http://twitter.com/rdanielday">Twitter</a>.
       </p>
       <p>
         Weather data provided by <a href="http://www.openweathermap.org">OpenWeatherMap</a>
